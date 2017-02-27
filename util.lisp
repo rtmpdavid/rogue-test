ae05883dffa9511c52c6ff8fd3413ec929195be4
David Selivanov <rtmpdavid@gmail.com>
2017-02-27 11:05:21 +0300

    Add stuff to util

--- util.lisp
+++ util.lisp
@@ -6,15 +6,35 @@
 (defun x (pos)
   (first pos))
 
+(defun (setf x) (value seq)
+  (setf (first seq) value))
+
 (defun y (pos)
   (second pos))
+
+(defun (setf y) (value seq)
+  (setf (second seq) value))
 
 (defun w (dim)
   (first dim))
 
+(defun (setf w) (value seq)
+  (setf (first seq) value))
+
 (defun h (dim)
   (second dim))
 
+(defun (setf h) (value seq)
+  (setf (second seq) value))
+
+(defun half-w (dims)
+  (truncate (w dims) 2))
+
+(defun half-h (dims)
+  (truncate (h dims) 2))
+
+(defun clamp (n a b)
+  (max (min n b) a))
 
 (defmacro compose (n-args &rest predicates)
   (let ((args (loop for i from 0 to (1- n-args)
@@ -22,3 +42,6 @@
     `#'(lambda ,args
 	 (and ,@(loop for pred in predicates
 		      collect `(funcall ,pred ,@args))))))
+
+(defun empty-p (sequence)
+  (zerop (length sequence)))
