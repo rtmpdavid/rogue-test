d7366ff504ab6c28aef0fdbfe34c52c2a4ca5e93
David Selivanov <rtmpdavid@gmail.com>
2017-03-02 16:07:08 +0300

    Add utils

--- util.lisp
+++ util.lisp
@@ -3,6 +3,9 @@
 (defun in-range (n origin range)
   (<= origin n (+ origin range)))
 
+(defun p (x y)
+  (list x y))
+
 (defun x (pos)
   (first pos))
 
@@ -14,6 +17,14 @@
 
 (defun (setf y) (value seq)
   (setf (second seq) value))
+
+(defun pos+ (a b)
+  (list (+ (x a) (x b))
+	(+ (y a) (y b))))
+
+(defun pos- (a b)
+  (list (- (x a) (x b))
+	(- (y a) (y b))))
 
 (defun w (dim)
   (first dim))
@@ -45,3 +56,52 @@
 
 (defun empty-p (sequence)
   (zerop (length sequence)))
+
+(defun positive-p (number)
+  (> 0 number))
+
+(defun negative-p (number)
+  (< 0 number))
+
+;; (defun prog-or (&rest forms)
+;;   "evaluate forms in order, then return with non-nil if at least one of the forms has returned non-nil"
+;;   )
+
+(defun direction-delta (direction)
+  (case direction
+    (:up (list 0 -1))
+    (:down (list 0 1))
+    (:left (list -1 0))
+    (:right (list 1 0))
+    (:up-left (list -1 -1))
+    (:up-right (list 1 -1))
+    (:bot-left (list -1 1))
+    (:bot-right (list 1 1))
+    (:nop (list 0 0))))
+
+(defun direction-opposite (direction)
+  (case direction
+    (:up :down)
+    (:down :up)
+    (:left :right)
+    (:right :left )
+    (:up-left :bot-right)
+    (:up-right :bot-left)
+    (:bot-left :up-right )
+    (:bot-right :up-left)))
+
+(defvar *axises* '(:up :down :left :right))
+(defvar *diags* '(:up-left :bot-right :up-right :bot-left))
+(defvar *dirs-nonzero* (append *axises* *diags*))
+(defvar *dirs* (append (list :nop) *dirs-nonzero*))
+
+(defun pos+dir (pos dir)
+  (pos+ pos (direction-delta dir)))
+
+(defun pos-dir (pos dir)
+  (pos- pos (direction-delta dir)))
+
+(defun random-range (a b)
+  (if (= a b) a
+      (progn (assert (< a b))
+	     (+ (random (1+ (- b a))) a))))
